ed9f862d0886302af9e371722a8b7cac7f33b74b
Petr Tsopa <petrtsopa03@gmail.com>
2024-06-23 01:54:13 +0300

    fix issues

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import ipaddress
 import argparse
+import pythonping
 
 
 def is_valid_ip(ip):
@@ -12,71 +13,43 @@ def is_valid_ip(ip):
         return False
 
 
-def iswin():
-    return sys.platform.startswith("win")
-
-
-def islinux():
-    return sys.platform.startswith("linux")
-
-
-def ismac():
-    return sys.platform.startswith("darwin")
-
-
-def single_ping_args(ip):
-    args = ["ping", ip]
-    if iswin():
-        args.extend(["-n", "1"])
-    else:
-        args.extend(["-c", "1"])
-    return args
-
-
-def ping_host(ip):
+def try_ping_host(ip, mtu):
+    responses = iter(pythonping.ping(ip, size=mtu, df=True, count=1))
     try:
-        subprocess.check_output(single_ping_args(ip), stderr=subprocess.DEVNULL)
-        return True
-    except subprocess.CalledProcessError:
+        response = next(responses)
+        return response.success
+    except StopIteration:
         return False
 
 
-def find_mtu(ip, min_mtu, max_mtu, step):
+MIN_MTU = 68
+HEADER_SIZE = 28
+
+
+def find_mtu(ip, max_mtu):
     if not is_valid_ip(ip):
         print("Invalid IP address.")
         return
 
-    if not ping_host(ip):
+    if not try_ping_host(ip, MIN_MTU):
         print("Destination is unreachable.")
         return
-    
-    if min_mtu < 0 or max_mtu < 0 or step <= 0:
-        print("MTU values must be positive.")
-        return
-    
-    if min_mtu > max_mtu:
-        print("Minimum MTU value cannot be greater than maximum.")
+
+    if max_mtu - HEADER_SIZE < MIN_MTU:
+        print(f"Max MTU value must be greater than {MIN_MTU + HEADER_SIZE}.")
         return
 
-    good_mtu = 0
-    for mtu in range(min_mtu, max_mtu, step):
-        try:
-            args = single_ping_args(ip)
-            if iswin():
-                args.extend(["-f", "-l", str(mtu)])
-            elif islinux():
-                args.extend(["-M", "do", "-s", str(mtu)])
-            elif ismac():
-                args.extend(["-D", "-s", str(mtu)])
-            subprocess.check_output(args, stderr=subprocess.DEVNULL)
-            good_mtu = mtu
-        except subprocess.CalledProcessError:
-            if good_mtu:
-                print(f"Maximum MTU value: {good_mtu}")
-            else:
-                print("Failed to find MTU value.")
-            return
-    print(f"Upper limit of range reached: {good_mtu}")
+    l = MIN_MTU
+    r = max_mtu - HEADER_SIZE + 1
+    while r - l > 1:
+        m = (l + r) // 2
+        if try_ping_host(ip, m):
+            l = m
+        else:
+            r = m
+
+    print(f"Maximum MTU value: {l + HEADER_SIZE}")
+    return l
 
 
 def main():
@@ -90,15 +63,6 @@ def main():
     )
 
     parser.add_argument(
-        '-n',
-        '--min',
-        dest='min',
-        default=500,
-        help='Min MTU value to check',
-        type=int
-    )
-
-    parser.add_argument(
         '-x',
         '--max',
         dest='max',
@@ -107,17 +71,8 @@ def main():
         type=int
     )
 
-    parser.add_argument(
-        '-s',
-        '--step',
-        dest='step',
-        default=10,
-        help='Step value',
-        type=int
-    )
-
     args = parser.parse_args()
-    find_mtu(args.ip, args.min, args.max, args.step)
+    find_mtu(args.ip, args.max)
 
 
 if __name__ == "__main__":
